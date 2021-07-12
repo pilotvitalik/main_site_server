50f987c77c4c363aac5f85f462b1c7b2f608f1b4
pilotvitalik <itvkip@yandex.ru>
2021-07-12 23:08:33 +0300

    Добавлен функционал подсчета кол-ва строк в таблице route и динамический pointid

--- nextjs/route_trip/modules/addMySQL.js
+++ nextjs/route_trip/modules/addMySQL.js
@@ -15,17 +15,32 @@ exports.add = function(req, res) {
   })
   req.on('end', () => {
     body = JSON.parse(body);
-    insert(body, res)
+    calcRowsTable(body, res)
   })
 };
 
-function insert(data, res){
+function calcRowsTable(data, res){
+  let count;
   res.setHeader('Content-Type', 'text/plain');
   connection.query(
       `SELECT COUNT(*) FROM route`,
-    //`INSERT INTO route(point, distance, time, speed, pointid) VALUES ('${data.title}', '${data.distance}', '${data.time}', '${data.speed}', '${data.pointId}')`,
     function(err, results, fields) {
-        console.log(results);
+      if (err) {
+        console.log(err);
+        res.end('Возникла внутренняя ошибка сервера');
+        return false;
+      }
+      count = results[0]['COUNT(*)'];
+      insert(data, count + 1, res);
+    }
+  );
+}
+
+function insert(data, count, res){
+  const queryString = `'${data.title}', '${data.distance}', '${data.time}', '${data.speed}', 'route_${count}'`;
+  connection.query(
+    `INSERT INTO route(point, distance, time, speed, pointid) VALUES (${queryString})`,
+    function(err, results, fields) {
       if (err) {
         console.log(err);
         res.end('Возникла внутренняя ошибка сервера');
