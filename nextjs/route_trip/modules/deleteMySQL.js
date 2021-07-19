baea869e22d2862cfba27da2b9036a51aec2c9cb
Vitaliy <12345678t>
2021-07-19 15:37:48 +0300

    При удалении крайней записи, обновляется значение Auto_increment

--- nextjs/route_trip/modules/deleteMySQL.js
+++ nextjs/route_trip/modules/deleteMySQL.js
@@ -20,17 +20,43 @@ exports.delete = function(req, res) {
 };
 
 function deleteRowTable(data, res){
-  console.log(data)
+  let count = 0;
   connection.query(
     `DELETE FROM route WHERE id=${data.id}`,
     function(err, results, fields) {
-      console.log(results)
       if (err) {
         console.log(err);
         res.end('Возникла внутренняя ошибка сервера');
         return false;
       }
-      res.end('Данные обновлены');
+      count = calcRows(res);
     }
+  );
+}
+
+function calcRows(res){
+  connection.query(
+      `SELECT COUNT(*) FROM route`,
+      function(err, results, fields) {
+        if (err) {
+          res.end('Возникла внутренняя ошибка сервера');
+          return false;
+        }
+        count = results[0]['COUNT(*)'];
+        zeroingAutoIncrement(count + 1, res);
+      }
+  );
+}
+
+function zeroingAutoIncrement(count, res){
+  connection.query(
+    `ALTER TABLE route AUTO_INCREMENT = ${count}`,
+      function(err, result, fields){
+          if (err) {
+              res.end('Возникла внутренняя ошибка сервера');
+              return false;
+          }
+          res.end('Данные обновлены');
+      }
   );
 }
