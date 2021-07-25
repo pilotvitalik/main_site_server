ec8673d4e11f08b934b37fe066568ce7f803a883
pilotvitalik <itvkip@yandex.ru>
2021-07-25 22:31:46 +0300

    Исправлен баг при подсчете времени после возобновления паузы

--- nextjs/route_trip/modules/pause.js
+++ nextjs/route_trip/modules/pause.js
@@ -25,10 +25,10 @@ exports.pause = function(req, res) {
 };
 
 function defineMinId(res){
-  let startId = 'SELECT MIN(id) FROM route NATURAL JOIN cross_point WHERE isChecked=false';
+  let startId = 'SELECT MAX(id) FROM route NATURAL JOIN cross_point WHERE isChecked=true';
   connection.promise().query(startId)
     .then (([rows, fields]) => {
-      globalObj.minId = rows[0]['MIN(id)'];
+      globalObj.maxId = rows[0]['MAX(id)'];
       if (globalObj.status){
         setPause(res); 
         return false;
@@ -40,7 +40,7 @@ function defineMinId(res){
 
 function setPause(res){
   res.setHeader('Content-Type', 'text/plain');
-  connection.promise().query(`UPDATE route SET pause=${globalObj.status} WHERE id=${globalObj.minId}`)
+  connection.promise().query(`UPDATE point_time SET time_start_pause=${globalObj.time} WHERE point_id=${globalObj.maxId}`)
     .then (([rows, fields]) => {
       res.end(JSON.stringify({status: 'Маршрут на паузе'}));
     })
@@ -48,20 +48,25 @@ function setPause(res){
 }
 
 function defineForecastTime(res){
-  let getForecastTime = `SELECT time FROM point_time WHERE point_id=${globalObj.minId}`;
-  connection.promise().query(getForecastTime)
+  let getStartPause = `SELECT time_start_pause FROM point_time WHERE point_id=${globalObj.maxId}`;
+  connection.promise().query(getStartPause)
     .then (([rows, fields]) => {
-      globalObj.forecast = Date.parse(rows[0]['time']);
+      globalObj.startPause = rows[0]['time_start_pause'];
       removePause(res);
     })
   .catch(console.log())
 }
 
 function removePause(res){
-  const diff = globalObj.forecast - globalObj.time;
-  const updTime = new Date(globalObj.forecast + diff);
-  const updForecast = `UPDATE point_time SET time='${updTime}' WHERE point_id=${globalObj.minId}`;
-  formatTime(updTime, res);
+  let getForecastTime = `SELECT time FROM point_time WHERE point_id=${globalObj.maxId + 1}`;
+  const diff = globalObj.time - globalObj.startPause;
+  connection.promise().query(getForecastTime)
+    .then (([rows, fields]) => {
+      globalObj.forecast = Date.parse(rows[0]['time']);
+      let updTime = new Date(globalObj.forecast + diff);
+      formatTime(updTime, res);
+    })
+  .catch(console.log())
 }
 
 function formatTime(updTime, res){
@@ -71,7 +76,7 @@ function formatTime(updTime, res){
     let numberMonth = (String(updTime.getMonth()).length === 1) ? `0${updTime.getMonth()}` : updTime.getMonth();
     let year = String(updTime.getFullYear()).slice(2);
     let finalString =`${hour}:${minutes} ${dateMonth}.${numberMonth}.${year}`;
-    let editRow = `UPDATE format_time SET time='${finalString}' WHERE time_id=${globalObj.minId}`;
+    let editRow = `UPDATE format_time SET time='${finalString}' WHERE time_id=${globalObj.maxId + 1}`;
     connection.promise().query(editRow)
         .then (([rows, fields]) => {
           mysqlGet.get(res);
